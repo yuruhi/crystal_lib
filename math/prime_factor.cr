c006360820ba343abc86e71ec61ffa2d614887a3
yuruhi <yuruhiya@gmail.com>
2020-06-06 19:58:45 +0900

    add 'prime_factor.cr'

---
+++ math/prime_factor.cr
@@ -0,0 +1,16 @@
+struct Int
+  def prime_factor : Array(Tuple(self, Int32))
+    res = Array(Tuple(self, Int32)).new
+    n = self
+    typeof(self).new(2).upto(Math.sqrt(self).ceil) do |i|
+      c = 0
+      while n % i == 0
+        n //= i
+        c += 1
+      end
+      res << {i, c} if c != 0
+    end
+    res << {n, 1} if n != 1
+    res
+  end
+end
