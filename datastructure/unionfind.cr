cb70ece39a05eea372141c19b66e036965ad8475
yuruhi <yuruhiya@gmail.com>
2020-06-23 08:06:35 +0900

    add 'UnionFind.uf'

---
+++ datastructure/unionfind.cr
@@ -0,0 +1,29 @@
+class UnionFind
+  @d : Array(Int32)
+
+  def initialize(n : Int32)
+    @d = Array.new(n, -1)
+  end
+
+  def root(x : Int32)
+    @d[x] < 0 ? x : (@d[x] = root(@d[x]))
+  end
+
+  def unite(x : Int32, y : Int32)
+    x = root(x)
+    y = root(y)
+    return false if x == y
+    x, y = y, x if @d[x] > @d[y]
+    @d[x] += @d[y]
+    @d[y] = x
+    true
+  end
+
+  def same?(x : Int32, y : Int32)
+    root(x) == root(y)
+  end
+
+  def size(x : Int32)
+    -@d[root(x)]
+  end
+end
