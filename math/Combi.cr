ad65806d9fce9694b159744fa9133fea8b95ae57
yuruhi <yuruhiya@gmail.com>
2020-10-21 21:58:57 +0900

    fix

--- math/Combi.cr
+++ math/Combi.cr
@@ -1,32 +1,32 @@
 # description : calculate combination
 class Comb(T)
-  def initialize(@Max = 300009)
-    @fac = Array(T).new(@Max, T.zero)
+  def initialize(@Max = 400009)
+    @factorial = Array(T).new(@Max, T.zero)
     @finv = Array(T).new(@Max, T.zero)
     @inv = Array(T).new(@Max, T.zero)
-    @fac[0] = @fac[1] = T.new(1)
+    @factorial[0] = @factorial[1] = T.new(1)
     @finv[0] = @finv[1] = T.new(1)
     @inv[1] = T.new(1)
     (2...@Max).each do |i|
-      @fac[i] = @fac[i - 1] * i
+      @factorial[i] = @factorial[i - 1] * i
       @inv[i] = -@inv[T.mod % i] * (T.mod / i)
       @finv[i] = @finv[i - 1] * @inv[i]
     end
   end
 
-  def p(n, r)
-    (n < r || n < 0 || r < 0) ? 0 : @fac[n] * @finv[n - r]
+  def permutation(n, r)
+    (n < r || n < 0 || r < 0) ? T.zero : @factorial[n] * @finv[n - r]
   end
 
-  def c(n, r)
-    (n < r || n < 0 || r < 0) ? 0 : @fac[n] * @finv[r] * @finv[n - r]
+  def combination(n, r)
+    (n < r || n < 0 || r < 0) ? T.zero : @factorial[n] * @finv[r] * @finv[n - r]
   end
 
-  def h(n, r)
-    (n < 0 || r < 0) ? 0 : r == 0 ? 1 : c(n + r - 1, r)
+  def homogeneous_product(n, r)
+    (n < 0 || r < 0) ? T.zero : r == T.zero ? T.new(1) : combination(n + r - 1, r)
   end
 
-  def fact(n)
-    @fac[n]
+  def factorial(n)
+    @factorial[n]
   end
 end
