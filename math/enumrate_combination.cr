4bf35f3cc12911e85353c30bf76bd7c3f8a56dca
yuruhi <yuruhiya@gmail.com>
2020-10-24 14:01:07 +0900

    add enumrate_combination.cr

---
+++ math/enumrate_combination.cr
@@ -0,0 +1,12 @@
+def enumrate_combination(n)
+  res = Array.new(n + 1) { Array.new(n + 1, 0i64) }
+  (0..n).each do |i|
+    res[i][0] = res[i][i] = 1
+  end
+  (1..n).each do |i|
+    (1...i).each do |j|
+      res[i][j] = res[i - 1][j - 1] + res[i - 1][j]
+    end
+  end
+  res
+end
