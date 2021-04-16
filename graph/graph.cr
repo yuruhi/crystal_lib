31bf3e23fa5ffd9ecf647eafb6e6dc3d5b8b325c
yuruhi <yuruhiya@gmail.com>
2021-04-16 21:59:20 +0900

    add Edge2(T)#reverse add Graph(T)#add_edge_directed add Graph(T)#[] add Graph(T)#each_edge add Graph(T)#edges

--- graph/graph.cr
+++ graph/graph.cr
@@ -13,6 +13,10 @@ struct Edge2(T)
 
   def initialize(@from : Int32, @to : Int32, @cost : T)
   end
+
+  def reverse
+    Edge2(T).new(to, from, cost)
+  end
 end
 
 class Graph(T)
@@ -35,7 +39,34 @@ class Graph(T)
 
   def add_edge(i : Int32, j : Int32, cost : T)
     raise IndexError.new unless 0 <= i < size
+    raise IndexError.new unless 0 <= j < size
     graph[i] << Edge(T).new(j, cost)
     graph[j] << Edge(T).new(i, cost)
+  end
+
+  def add_edge_directed(i : Int32, j : Int32, cost : T)
+    raise IndexError.new unless 0 <= i < size
+    raise IndexError.new unless 0 <= j < size
+    graph[i] << Edge(T).new(j, cost)
+  end
+
+  def [](i : Int32)
+    graph[i]
+  end
+
+  def each_edge : Nil
+    (0...size).each do |v|
+      graph[v].each do |edge|
+        yield Edge2(T).new(v, edge.to, edge.cost)
+      end
+    end
+  end
+
+  def edges : Array(Edge2(T))
+    result = [] of Edge2(T)
+    each_edge do |edge|
+      result << edge
+    end
+    result
   end
 end
