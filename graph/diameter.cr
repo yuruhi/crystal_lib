b3e02f22c536e07f60ad81a981228be61024e85a
yuruhi <yuruhiya@gmail.com>
2021-04-25 14:57:45 +0900

    fix diameter and add test

--- graph/diameter.cr
+++ graph/diameter.cr
@@ -1,10 +1,10 @@
 require "./graph"
-require "./bfs"
+require "./tree_distance"
 
 class Graph(T)
   def diameter : {T, Int32, Int32}
-    v = bfs!(0).each_with_index.max[1]
-    dist, u = bfs!(v).each_with_index.max
+    v = tree_distance(0).each_with_index.max[1]
+    dist, u = tree_distance(v).each_with_index.max
     {dist, v, u}
   end
 end
---
+++ test/diameter.test.cr
@@ -0,0 +1,8 @@
+# verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/GRL_5_A
+require "graph/diameter"
+n = read_line.to_i
+g = Graph.new n, (1..n - 1).map {
+  a, b, c = read_line.split.map(&.to_i)
+  Edge2.new(a, b, c.to_i)
+}, undirected: true
+puts g.diameter[0]
