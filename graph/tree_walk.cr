312181902a29ac2d2867270ec8955c6dce83d95a
yuruhi <yuruhiya@gmail.com>
2021-05-12 18:57:24 +0900

    delete in_order

--- graph/tree_walk.cr
+++ graph/tree_walk.cr
@@ -32,21 +32,4 @@ class Graph(T)
     end
     result
   end
-
-  def in_order(root : Int32)
-    raise ArgumentError.new "graph is not binary tree" unless @graph.all? { |v| v.size <= 2 }
-    result = Array(Int32).new(size)
-    stack = Deque(Int32).new
-    vertex = root
-    while vertex || !stack.empty?
-      while vertex
-        stack << vertex
-        vertex = self[vertex][0]?.try(&.to)
-      end
-      vertex = stack.pop
-      result << vertex
-      vertex = self[vertex][1]?.try(&.to)
-    end
-    result
-  end
 end
