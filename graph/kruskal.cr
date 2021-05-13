0a31b8181f123c6f1e006963039f296de3d313be
yuruhi <yuruhiya@gmail.com>
2021-05-13 21:15:03 +0900

    fix

--- graph/kruskal.cr
+++ graph/kruskal.cr
@@ -1,9 +1,9 @@
 require "./graph"
 require "../datastructure/UnionFind"
 
-def kruskal(n : Int32, edge : Array(Edge2(T))) forall T
+def kruskal(n : Int32, edges : Array(Edge2(T))) forall T
   uf = UnionFind.new(n)
-  edge.sort.sum do |edge|
+  edges.sort.sum do |edge|
     if uf.unite(edge.from, edge.to)
       edge.cost
     else
