9e96cc7590ce789f2f406ee97117c877c75d49b4
yuruhi <yuruhiya@gmail.com>
2021-05-27 15:29:27 +0900

    add test for CulSum

---
+++ test/CulSum.test.cr
@@ -0,0 +1,9 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/static_range_sum
+require "../datastructure/CulSum"
+n, q = read_line.split.map(&.to_i)
+a = read_line.split.map(&.to_i64)
+sum = CulSum.new(a)
+q.times do
+  l, r = read_line.split.map(&.to_i)
+  puts sum[l...r]
+end
