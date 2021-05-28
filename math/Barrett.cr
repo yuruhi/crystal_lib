c94d942cb1251938541e0885adc7f1ea23e3b499
yuruhi <yuruhiya@gmail.com>
2021-05-28 13:34:58 +0900

    add Barrett

---
+++ math/Barrett.cr
@@ -0,0 +1,17 @@
+struct Barrett
+  getter mod : UInt32
+  getter inv : UInt64
+
+  def initialize(@mod)
+    @inv = UInt64::MAX // @mod &+ 1
+  end
+
+  def mul(a : UInt32, b : UInt32) : UInt32
+    z = a.to_u64
+    z &*= b
+    x = ((z.to_u128 &* @inv) >> 64).to_u64!
+    v = (z &- x &* @mod).to_u32!
+    v &+= @mod if @mod <= v
+    v
+  end
+end
