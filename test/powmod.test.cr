20f9fcbf06a0fa51f0ec8aa232991d1e24cc5ce6
yuruhi <yuruhiya@gmail.com>
2021-05-28 14:11:13 +0900

    fix test

--- test/powmod.test.cr
+++ test/powmod.test.cr
@@ -1,4 +1,5 @@
 # verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_B
 require "../math/Mint"
+static_modint(Mint, 1000000007)
 a, b = read_line.split.map(&.to_i)
 puts Mint.new(a)**b
