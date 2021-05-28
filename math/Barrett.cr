2bd016060f677b1c7944804941f0c7c6df2c4c14
yuruhi <yuruhiya@gmail.com>
2021-05-28 16:53:50 +0900

    fix

--- math/Barrett.cr
+++ math/Barrett.cr
@@ -7,8 +7,7 @@ struct Barrett
   end
 
   def mul(a : UInt32, b : UInt32) : UInt32
-    z = a.to_u64
-    z &*= b
+    z = a.to_u64 &* b
     x = ((z.to_u128 &* @inv) >> 64).to_u64!
     v = (z &- x &* @mod).to_u32!
     v &+= @mod if @mod <= v
