fd7d91358cdc0d1cf17fab79002e534525f2ca4b
yuruhi <yuruhiya@gmail.com>
2021-06-20 14:07:00 +0900

    add DynamicMint

---
+++ math/DynamicMint.cr
@@ -0,0 +1,126 @@
+require "./Barrett"
+require "../atcoder/Math"
+
+struct DynamicMint
+  @@bt : Barrett = Barrett.new(998244353u32)
+  alias Mint = DynamicMint
+
+  def self.mod : Int32
+    @@bt.mod.to_i
+  end
+
+  def self.mod=(m : Int32)
+    raise ArgumentError.new unless 1 <= m
+    @@bt = Barrett.new(m.to_u32)
+  end
+
+  def self.raw(v : Int32)
+    result = Mint.new
+    result.value = v
+    result
+  end
+
+  def self.raw(v)
+    result = Mint.new
+    result.value = v.to_i!
+    result
+  end
+
+  def self.zero
+    Mint.new
+  end
+
+  getter value : Int32
+
+  protected def value=(v : Int32)
+    @value = v
+  end
+
+  def initialize
+    @value = 0
+  end
+
+  def initialize(x : self)
+    @value = x.value
+  end
+
+  def initialize(x : Int)
+    @value = x.to_i % Mint.mod
+  end
+
+  def ==(v : self)
+    value == v.value
+  end
+
+  def ==(v)
+    value == v
+  end
+
+  def +
+    self
+  end
+
+  def -
+    Mint.raw value == 0 ? 0 : Mint.mod - value
+  end
+
+  def +(v)
+    x = value + Mint.new(v).value
+    x -= Mint.mod if x >= Mint.mod
+    Mint.raw x
+  end
+
+  def -(v)
+    x = value - Mint.new(v).value
+    x += Mint.mod if x < 0
+    Mint.raw x
+  end
+
+  def *(v)
+    Mint.raw @@bt.mul(value.to_u!, Mint.new(v).value.to_u!)
+  end
+
+  def /(v)
+    self * Mint.new(v).inv
+  end
+
+  def //(v)
+    self / v
+  end
+
+  def **(exponent : Int)
+    t, res = self, Mint.raw(1)
+    while exponent > 0
+      res *= t if exponent & 1 == 1
+      t *= t
+      exponent >>= 1
+    end
+    res
+  end
+
+  def inv
+    Mint.raw AtCoder::Math.inv_mod(value, Mint.mod)
+  end
+
+  def succ
+    Mint.raw value == Mint.mod - 1 ? 0 : value + 1
+  end
+
+  def pred
+    Mint.raw value == 0 ? Mint.mod - 1 : value - 1
+  end
+
+  def abs
+    self
+  end
+
+  def to_i64 : Int64
+    value.to_i64
+  end
+
+  {% for op in %w[< <= > >=] %}
+    def {{op.id}}(other)
+      raise NotImplementedError.new({{op}})
+    end
+  {% end %}
+end
---
+++ spec/DynamicMint_spec.cr
@@ -0,0 +1,167 @@
+require "../math/DynamicMint"
+
+alias M = DynamicMint
+init = 998244353
+
+macro check_binary_operator(mod, op)
+	%mod = {{mod}}
+	M.mod = %mod
+	(0...%mod).each do |x|
+		(0...%mod).each do |y|
+			(M.new(x) {{op.id}} M.new(y)).should eq (x {{op.id}} y) % %mod
+		end
+	end
+end
+
+macro check_method(mod, method)
+  {1, 60, 1009, 1000003}.each do |mod|
+    M.mod = mod
+    (0...mod).each do |x|
+      M.new(x).{{method}}.should eq x.{{method}} % mod
+    end
+  end
+end
+
+describe "DynamicModint" do
+  it ".mod and .setmod" do
+    M.mod.should eq init
+    M.mod = 42
+    M.mod.should eq 42
+    M.mod = Int32::MAX
+    M.mod.should eq Int32::MAX
+    expect_raises(ArgumentError) { M.mod = 0 }
+    expect_raises(ArgumentError) { M.mod = -1 }
+  end
+
+  it ".zero" do
+    M.mod = 60
+    M.zero.should eq 0
+  end
+
+  it "#==(x)" do
+    M.mod = 60
+    (M.new(0) == M.new(0)).should be_true
+    (M.new(0) == M.new(60)).should be_true
+    (M.new(0) == 0).should be_true
+    (M.new(0) == 1).should be_false
+    (M.new(60) == 0).should be_true
+    (M.new(60) == 60).should be_false
+  end
+
+  it "#+" do
+    M.mod = init
+    (+M.new(1)).should eq 1
+    (+M.new(init)).should eq 0
+  end
+
+  it "#-" do
+    M.mod = init
+    x = M.new(1)
+    (-x).should eq init - 1
+    x = M.new(0)
+    (-x).value.should eq 0
+
+    M.mod = 3
+    (-M.new(1)).should eq 2
+  end
+
+  it "#+(x)" do
+    check_binary_operator(1, "+")
+    check_binary_operator(60, "+")
+    check_binary_operator(1009, "+")
+  end
+
+  it "#-(x)" do
+    check_binary_operator(1, "-")
+    check_binary_operator(60, "-")
+    check_binary_operator(1009, "-")
+  end
+
+  it "#*(x)" do
+    check_binary_operator(1, "*")
+    check_binary_operator(60, "*")
+    check_binary_operator(1009, "*")
+  end
+
+  it "#/(x)" do
+    {1, 60, 1009}.each do |mod|
+      M.mod = mod
+      (0...mod).each do |x|
+        (0...mod).each do |y|
+          next unless y.gcd(mod) == 1
+          z = M.new(x) / y
+          (z * y).should eq x
+        end
+      end
+    end
+  end
+
+  it "#//(x)" do
+    {1, 60, 1009}.each do |mod|
+      M.mod = mod
+      (0...mod).each do |x|
+        (0...mod).each do |y|
+          next unless y.gcd(mod) == 1
+          z = M.new(x) // y
+          (z * y).should eq x
+        end
+      end
+    end
+  end
+
+  it "#**(x)" do
+    {1, 60, 1009}.each do |mod|
+      M.mod = mod
+      (0i64...mod.to_i64).each do |x|
+        0i64.step(to: 10i64**18, by: 10i64**16) do |e|
+          (M.new(x) ** e).should eq AtCoder::Math.pow_mod(x, e, mod)
+        end
+      end
+    end
+  end
+
+  it "#inv" do
+    {1, 60, 1009, 1000003}.each do |mod|
+      M.mod = mod
+      (1...mod).each do |x|
+        next unless x.gcd(mod) == 1
+        (M.new(x).inv * x).should eq 1
+      end
+    end
+  end
+
+  it "#succ" do
+    check_method(1, succ)
+    check_method(60, succ)
+    check_method(1009, succ)
+    check_method(1000003, succ)
+  end
+
+  it "#pred" do
+    check_method(1, pred)
+    check_method(60, pred)
+    check_method(1009, pred)
+    check_method(1000003, pred)
+  end
+
+  it "#abs" do
+    check_method(1, abs)
+    check_method(60, abs)
+    check_method(1009, abs)
+    check_method(1000003, abs)
+  end
+
+  it "#to_i64" do
+    check_method(1, to_i64)
+    check_method(60, to_i64)
+    check_method(1009, to_i64)
+    check_method(1000003, to_i64)
+  end
+
+  it "compare" do
+    expect_raises(NotImplementedError) { M.new(0) < 0 }
+    expect_raises(NotImplementedError) { M.new(0) <= 0 }
+    expect_raises(NotImplementedError) { M.new(0) > 0 }
+    expect_raises(NotImplementedError) { M.new(0) >= 0 }
+  end
+end
