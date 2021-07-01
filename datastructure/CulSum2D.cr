501161752fc0b41b930e23eb37aa8a226c24dd41
yuruhi <yuruhiya@gmail.com>
2021-07-01 18:48:43 +0900

    fix

--- datastructure/CulSum2D.cr
+++ datastructure/CulSum2D.cr
@@ -6,7 +6,7 @@ class CulSum2D(T)
     @height = a.size
     raise ArgumentError.new unless height > 0
     @width = a[0].size
-    raise ArgumentError.new unless a.map(&.size).all?(width)
+    raise ArgumentError.new unless a.all? { |b| b.size == width }
 
     @s = Array(Array(T)).new(height + 1) { Array.new(width + 1, T.zero) }
     (0...height).each do |i|
@@ -27,18 +27,40 @@ class CulSum2D(T)
     end
   end
 
-  def [](y_start, y_count, x_start, x_count)
-    @s[y_start + y_count][x_start + x_count] - @s[y_start + y_count][x_start] -
-      @s[y_start][x_start + x_count] + @s[y_start][x_start]
+  def []?(y_start : Int, y_count : Int, x_start : Int, x_count : Int)
+    raise ArgumentError.new "Negative y_count: #{y_count}" unless y_count >= 0
+    raise ArgumentError.new "Negative x_count: #{x_count}" unless x_count >= 0
+    y_start += height if y_start < 0
+    x_start += width if x_start < 0
+
+    if 0 <= y_start <= height && 0 <= x_start <= width
+      y_count = Math.min(y_count, height - y_start)
+      x_count = Math.min(x_count, height - x_start)
+
+      @s[y_start + y_count][x_start + x_count] - @s[y_start + y_count][x_start] -
+        @s[y_start][x_start + x_count] + @s[y_start][x_start]
+    end
+  end
+
+  def []?(y_range : Range, x_range : Range)
+    ys, yc = Indexable.range_to_index_and_count(y_range, height) || raise IndexError.new
+    xs, xc = Indexable.range_to_index_and_count(x_range, width) || raise IndexError.new
+    self[ys, yc, xs, xc]?
+  end
+
+  def [](ys : Int, yc : Int, xs : Int, xc : Int)
+    self[ys, yc, xs, xc]? || raise IndexError.new
   end
 
   def [](y_range : Range, x_range : Range)
-    ys, yc = Indexable.range_to_index_and_count(y_range, height) || raise IndexError.new
-    xs, xc = Indexable.range_to_index_and_count(x_range, width) || raise IndexError.new
-    self[ys, yc, xs, xc]
+    self[y_range, x_range]? || raise IndexError.new
   end
 
   def to_a
-    (0...@n).map { |i| self[i..i] }
+    Array.new(height) do |i|
+      Array.new(width) do |j|
+        self[i, 1, j, 1]
+      end
+    end
   end
 end
