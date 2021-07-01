5f9c12da95239591ddf72f84cc62ad928ab46333
yuruhi <yuruhiya@gmail.com>
2021-07-01 18:58:22 +0900

    add Scanner

--- Scanner.cr
+++ Scanner.cr
@@ -1,21 +1,16 @@
 require "io/error"
 
 class Scanner
-  INSTANCE = new
-
-  def initialize(@io : IO = STDIN)
-  end
-
-  def s
-    peek = @io.peek.not_nil!
+  def self.s
+    peek = STDIN.peek.not_nil!
     if not_space = peek.index { |x| x != 32 && x != 10 }
       if index = peek.index(not_space) { |x| x == 32 || x == 10 }
         result = String.new(peek[not_space...index])
-        @io.skip(index + 1)
+        STDIN.skip(index + 1)
         result
       else
         result = String.new(peek[not_space..])
-        @io.skip_to_end
+        STDIN.skip_to_end
         result
       end
     else
@@ -23,7 +18,7 @@ class Scanner
     end
   end
 
-  def i
+  def self.i
     s.to_i
   end
 end
@@ -40,13 +35,15 @@ end
 
 macro input(type)
   {% if type.is_a?(Path) %}
-    {{type}}.new(Scanner::INSTANCE.s)
+    {{type}}.new(Scanner.s)
   {% elsif type.is_a?(Call) && type.args.size == 0 %}
     {% if Scanner.methods.includes?(type) %}
-      Scanner::INSTANCE.{{type.id}}
+      Scanner.{{type.id}}
     {% else %}
-      Scanner::INSTANCE.s.to_{{type.id}}
+      Scanner.s.to_{{type.id}}
     {% end %}
+  {% elsif type.name == "[]" %}
+    input_array("{{type.receiver}}", {{type.args}})
   {% else %}
     input_array("{{type.name}}", {{type.args}})
   {% end %}
