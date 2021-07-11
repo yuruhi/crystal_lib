c117681673656c40a80d3c0c08a212d05d699fdc
yuruhi <yuruhiya@gmail.com>
2021-07-11 13:09:31 +0900

    refactoring

--- src/dp/inversion.cr
+++ src/dp/inversion.cr
@@ -6,9 +6,7 @@ module DP
   def inversion(a : Array(T)) : Int64 forall T
     bit = BinaryIndexedTree(Int32).new(a.max + 1)
     a.each_with_index.sum(0i64) do |x, i|
-      (i - bit[0, x]).tap do
-        bit.add(x, 1)
-      end
+      bit[x + 1..].tap { bit.add(x, 1) }
     end
   end
 end
