34fc04a52f7bace4ab3e6e3aadb93a076b861abd
yuruhi <yuruhiya@gmail.com>
2021-07-11 10:52:18 +0900

    module DP

--- src/DP/knapsack.cr
+++ src/DP/knapsack.cr
@@ -1,19 +1,23 @@
-def knapsack(weight_limit : Int32, value : Array(Int), weight : Array(Int32))
-  raise ArgumentError.new unless value.size == weight.size
-  n = value.size
-  (0...n).each_with_object([typeof(value.first).zero] * weight_limit.succ) do |i, dp|
-    (0..weight_limit - weight[i]).each do |j|
-      dp[j + weight[i]] = Math.max(dp[j + weight[i]], dp[j] + value[i])
+module DP
+  extend self
+
+  def knapsack(weight_limit : Int32, value : Array(Int), weight : Array(Int32))
+    raise ArgumentError.new unless value.size == weight.size
+    n = value.size
+    (0...n).each_with_object([typeof(value.first).zero] * weight_limit.succ) do |i, dp|
+      (0..weight_limit - weight[i]).each do |j|
+        dp[j + weight[i]] = Math.max(dp[j + weight[i]], dp[j] + value[i])
+      end
     end
   end
-end
 
-def knapsack01(weight_limit : Int32, value : Array(Int), weight : Array(Int32))
-  raise ArgumentError.new unless value.size == weight.size
-  n = value.size
-  (0...n).each_with_object([typeof(value.first).zero] * weight_limit.succ) do |i, dp|
-    (0..weight_limit - weight[i]).reverse_each do |j|
-      dp[j + weight[i]] = Math.max(dp[j + weight[i]], dp[j] + value[i])
+  def knapsack01(weight_limit : Int32, value : Array(Int), weight : Array(Int32))
+    raise ArgumentError.new unless value.size == weight.size
+    n = value.size
+    (0...n).each_with_object([typeof(value.first).zero] * weight_limit.succ) do |i, dp|
+      (0..weight_limit - weight[i]).reverse_each do |j|
+        dp[j + weight[i]] = Math.max(dp[j + weight[i]], dp[j] + value[i])
+      end
     end
   end
 end
--- test/Knapsack01.test.cr
+++ test/Knapsack01.test.cr
@@ -2,4 +2,4 @@
 require "../src/DP/knapsack"
 n, limit = read_line.split.map(&.to_i)
 v, w = (1..n).map { read_line.split.map(&.to_i) }.transpose
-puts knapsack01(limit, v, w)[limit]
+puts DP.knapsack01(limit, v, w)[limit]
