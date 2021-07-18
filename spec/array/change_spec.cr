15a8453fd5192dbd8af8fd96482075ad5b47ea12
yuruhi <yuruhiya@gmail.com>
2021-07-18 19:56:57 +0900

    add Array#{chmin, chmax}(indexes, value)

--- spec/array/change_spec.cr
+++ spec/array/change_spec.cr
@@ -14,6 +14,18 @@ describe Array do
     a.should eq [0, 1, 0, 0]
   end
 
+  it "#chmin(indexes, value)" do
+    a = [[0, 1], [2, 3], [4, 5]]
+    a.chmin({1, 1}, 0).should be_true
+    a.should eq [[0, 1], [2, 0], [4, 5]]
+    a.chmin({0, 0}, 0).should be_false
+    a.should eq [[0, 1], [2, 0], [4, 5]]
+    a.chmin({2, 1}, 3).should be_true
+    a.should eq [[0, 1], [2, 0], [4, 3]]
+    a[1].chmin({0}, 42).should be_false
+    a.should eq [[0, 1], [2, 0], [4, 3]]
+  end
+
   it "#chmax(i, value)" do
     a = [0, 1, 2, 3]
     a.chmax(3, 4).should be_true
@@ -24,5 +36,17 @@ describe Array do
     a.should eq [0, 1, 4, 4]
     a.chmax(1, -10).should be_false
     a.should eq [0, 1, 4, 4]
+  end
+
+  it "#chmax(indexes, value)" do
+    a = [[0, 1], [2, 3], [4, 5]]
+    a.chmax({1, 1}, 10).should be_true
+    a.should eq [[0, 1], [2, 10], [4, 5]]
+    a.chmax({0, 0}, 0).should be_false
+    a.should eq [[0, 1], [2, 10], [4, 5]]
+    a.chmax({2, 1}, 11).should be_true
+    a.should eq [[0, 1], [2, 10], [4, 11]]
+    a[1].chmax({0}, -42).should be_false
+    a.should eq [[0, 1], [2, 10], [4, 11]]
   end
 end
--- src/array/change.cr
+++ src/array/change.cr
@@ -5,9 +5,25 @@ class Array(T)
     end
   end
 
+  protected def chmin(i : Int, *indexes, value)
+    self[i].chmin(*indexes, value: value)
+  end
+
+  def chmin(indexes : Tuple, value)
+    chmin(*indexes, value: value)
+  end
+
   def chmax(i : Int, value : T)
     (self[i] < value).tap do |f|
       self[i] = value if f
     end
+  end
+
+  protected def chmax(i : Int, *indexes, value)
+    self[i].chmax(*indexes, value: value)
+  end
+
+  def chmax(indexes : Tuple, value)
+    chmax(*indexes, value: value)
   end
 end
