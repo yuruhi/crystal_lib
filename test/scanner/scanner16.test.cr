ec8aa3d0c73190375f7023c3915cd0d8161ad6e8
yuruhi <yuruhiya@gmail.com>
2021-07-18 14:54:09 +0900

    fix ChainedCallWithNoBang

--- test/scanner/scanner16.test.cr
+++ test/scanner/scanner16.test.cr
@@ -1,4 +1,4 @@
 # verification-helper: PROBLEM https://yukicoder.me/problems/no/5
 require "../../src/scanner"
 l, range = input(i, 0...i)
-puts range.map { input(i) }.sort.count { |x| (l -= x) >= 0 }
+puts range.map { input(i) }.sort!.count { |x| (l -= x) >= 0 }
