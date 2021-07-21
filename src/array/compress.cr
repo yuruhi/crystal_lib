e20f6ca02b37ce5017840ad040cb19fdb4eac6ae
yuruhi <yuruhiya@gmail.com>
2021-07-21 16:11:26 +0900

    Array#compress(index)

--- src/array/compress.cr
+++ src/array/compress.cr
@@ -1,11 +1,11 @@
 class Array(T)
-  def compress(values : Array(T))
+  def compress(values : Array(T), *, index : Int = 0)
     map do |x|
-      values.bsearch_index { |y| y >= x }.not_nil!
+      index + values.bsearch_index { |y| y >= x }.not_nil!
     end
   end
 
-  def compress : Array(Int32)
-    compress(uniq.sort!)
+  def compress(*, index : Int = 0) : Array(Int32)
+    compress(uniq.sort!, index: index)
   end
 end
