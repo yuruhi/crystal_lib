2ea22bc801fca444c584a81b05c7935f8d5488ca
yuruhi <yuruhiya@gmail.com>
2021-07-23 17:04:09 +0900

    fix compile error

--- src/graph/components.cr
+++ src/graph/components.cr
@@ -3,13 +3,13 @@ require "../datastructure/union_find"
 
 module Graph(Edge, Edge2)
   # Returns `{components size, index, groups}`.
-  def components : {Int32, Array(Int32), Set(Set(Int32))}
+  def components : {Int32, Array(Int32), Array(Set(Int32))}
     uf = UnionFind.new(size)
     each do |edge|
       uf.unite(edge.from, edge.to)
     end
-    groups = uf.groups
-    id = Array.new(-1, n)
+    groups = uf.groups.to_a
+    id = Array.new(size, -1)
     groups.each_with_index do |group, i|
       group.each { |v| id[v] = i }
     end
