1c4b46623de2ff7e07838d3580638fead6eba851
yuruhi <yuruhiya@gmail.com>
2021-07-23 16:39:21 +0900

    refactor Graph#components

--- src/graph/components.cr
+++ src/graph/components.cr
@@ -1,26 +1,18 @@
 require "../graph"
+require "../datastructure/union_find"
 
 module Graph(Edge, Edge2)
   # Returns `{components size, index, groups}`.
-  def components
-    undirected = to_undirected
-    index = Array(Int32?).new(size, nil)
-    groups = [] of Set(Int32)
-    id = 0
-    size.times do |v|
-      next if index[v]
-      que = Deque{v}
-      groups << Set(Int32).new
-      while u = que.shift?
-        next if index[u]
-        index[u] = id
-        groups[id] << u
-        undirected[u].each do |edge|
-          que << edge.to if index[edge.to].nil?
-        end
-      end
-      id += 1
+  def components : {Int32, Array(Int32), Set(Set(Int32))}
+    uf = UnionFind.new(size)
+    each do |edge|
+      uf.unite(edge.from, edge.to)
     end
-    {id, index.map(&.not_nil!), groups}
+    groups = uf.groups
+    id = Array.new(-1, n)
+    groups.each_with_index do |group, i|
+      group.each { |v| id[v] = i }
+    end
+    {groups.size, id, groups}
   end
 end
