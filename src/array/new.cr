9a27c8b75389322f03eda25d9ab946441db2741d
yuruhi <yuruhiya@gmail.com>
2021-07-24 16:21:07 +0900

    delete tab

--- src/array/new.cr
+++ src/array/new.cr
@@ -2,16 +2,16 @@ class Array
   def self.new(sizes : Tuple(*T), initial_value) forall T
     {% begin %}
       {% for i in 0...T.size %} Array.new(sizes[{{i}}]) { {% end %}
-			initial_value
-	  	{% for i in 0...T.size %} } {% end %}
-		{% end %}
+      initial_value
+      {% for i in 0...T.size %} } {% end %}
+    {% end %}
   end
 
   def self.new(sizes : Tuple(*T), &block) forall T
     {% begin %}
       {% for i in 0...T.size %} Array.new(sizes[{{i}}]) { |%i{i}| {% end %}
-			yield({% for i in 0...T.size %} %i{i}, {% end %})
-	  	{% for i in 0...T.size %} } {% end %}
-		{% end %}
+      yield({% for i in 0...T.size %} %i{i}, {% end %})
+      {% for i in 0...T.size %} } {% end %}
+    {% end %}
   end
 end
