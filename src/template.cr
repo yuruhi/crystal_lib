bdd7dbc287f1940051ab38d17fed83426777307b
yuruhi <yuruhiya@gmail.com>
2021-08-02 16:24:13 +0900

    fix template

--- src/template.cr
+++ src/template.cr
@@ -1,9 +1,4 @@
-lib C
-  fun strtoll(s : UInt8*, p : UInt8**, b : Int32) : Int64
-end
-
-class String
-  def to_i64
-    C.strtoll(self, nil, 10)
-  end
-end
+require "./scanner"
+require "./tuple/times"
+require "./array/new"
+require "./array/change"
