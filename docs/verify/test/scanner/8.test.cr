067001df2fd831609db969540008ee7bbab70001
yuruhi <yuruhi@users.noreply.github.com>
2021-08-03 08:00:50 +0000

    deploy: 2e995f73f747f976ea750cf1a32c4b24456290e8

---
+++ docs/verify/test/scanner/8.test.cr
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <meta charset="utf-8">
+  <title>Redirecting&hellip;</title>
+  <link rel="canonical" href="/crystal_lib/docs/test/scanner/8.test.cr.html">
+  <script>location="/crystal_lib/docs/test/scanner/8.test.cr.html"</script>
+  <meta http-equiv="refresh" content="0; url=/crystal_lib/docs/test/scanner/8.test.cr.html">
+  <meta name="robots" content="noindex">
+  <h1>Redirecting&hellip;</h1>
+  <a href="/crystal_lib/docs/test/scanner/8.test.cr.html">Click here if you are not redirected.</a>
+</html>
