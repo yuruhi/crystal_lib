049ce09426ad8c9dfed0480c2008f51a919a5dd1
yuruhi <yuruhiya@gmail.com>
2021-08-08 12:21:06 +0900

    fix Array.new

--- src/array/new.cr
+++ src/array/new.cr
@@ -9,8 +9,8 @@ class Array
 
   def self.new(sizes : Tuple(*T), &block) forall T
     {% begin %}
-      {% for i in 0...T.size %} Array.new(sizes[{{i}}]) { |%i{i}| {% end %}
-      yield({% for i in 0...T.size %} %i{i}, {% end %})
+      {% for i in 0...T.size %} Array.new(sizes[{{i}}]) { |index{{i}}| {% end %}
+      yield({% for i in 0...T.size %} index{{i}}, {% end %})
       {% for i in 0...T.size %} } {% end %}
     {% end %}
   end
