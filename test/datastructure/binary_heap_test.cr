68ca7d5dff65525338565ee1de784ee5f75c90c1
yuruhi <yuruhiya@gmail.com>
2021-09-06 18:04:44 +0900

    fix

--- test/datastructure/binary_heap_test.cr
+++ test/datastructure/binary_heap_test.cr
@@ -1,7 +1,7 @@
 # verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_9_C
 require "../../src/datastructure/binary_heap"
 
-a = BinaryHeap(Int32).new
+a = BinaryHeap(Int32).new { |a, b| b <=> a }
 loop do
   case gets
   when .=~ /insert (\d+)/
