f8b66a4694f799e6a880a4e854606eb0ea9053a7
yuruhi <yuruhiya@gmail.com>
2021-09-11 19:41:35 +0900

    add `Set::RedBlackTree`

---
+++ benchmarks/datastructure/set/red_black_tree.cr
@@ -0,0 +1,57 @@
+require "benchmark"
+require "../../../src/datastructure/set/red_black_tree"
+
+class Foo
+  getter x
+  include Comparable(Foo)
+
+  def initialize(@x = 0)
+  end
+
+  def <=>(other : Foo)
+    x <=> other.x
+  end
+end
+
+R = Random.new(12345)
+
+def benchmark_add_delete(label, type : T.class, values) forall T
+  index = (0...values.size).to_a.shuffle Random.new(123)
+  Benchmark.ips do |x|
+    x.report(label) do
+      s = T.new
+      values.each { |x| s.add x }
+      index.each { |i| s.delete values[i] }
+    end
+  end
+end
+
+def benchmark_split(label, type : T.class, values, split_key) forall T
+  Benchmark.ips do |x|
+    x.report(label) do
+      s = T.new values
+      _, _ = s.split(split_key)
+    end
+  end
+end
+
+SInt32 = Set::RedBlackTree(Int32)
+SArray = Set::RedBlackTree(Array(Int32))
+SClass = Set::RedBlackTree(Foo)
+
+values3 = Array.new(10**3, &.itself)
+values6 = Array.new(10**6, &.itself)
+
+puts "-------- add, delete --------"
+benchmark_add_delete "Int32 1e3 sorted", SInt32, values3
+benchmark_add_delete "Int32 1e3       ", SInt32, values3.shuffle(R)
+benchmark_add_delete "Int32 1e6 sorted", SInt32, values6
+benchmark_add_delete "Int32 1e6       ", SInt32, values6.shuffle(R)
+benchmark_add_delete "Int32 1e3 * 1e3 ", SInt32, values6.map { |x| x % 1000 }.shuffle!(R)
+benchmark_add_delete "Array 1e6 * 1e2 ", SArray, Array.new(10**6) { Array.new(10**2) { R.rand(100) } }
+benchmark_add_delete "class 1e6       ", SClass, Array.new(10**6) { |i| Foo.new i }.shuffle(R)
+puts
+
+puts "-------- split --------"
+benchmark_split "Int32 5e5+5e5", SInt32, values6, 5_000_000
+benchmark_split "Int32 1e5+9e5", SInt32, values6, 1_000_000
---
+++ spec/datastructure/set/red_black_tree_spec.cr
@@ -0,0 +1,211 @@
+require "spec"
+require "../../../src/datastructure/set/red_black_tree"
+
+alias S = Set::RedBlackTree(Int32)
+
+def verify_dfs(node)
+  if node.nil_node?
+    node.left.nil_node?.should be_true
+    node.right.nil_node?.should be_true
+  end
+
+  if node.left.node?
+    node.key.should be > node.left.key
+    (node.red? && node.left.red?).should be_false
+    node.left.parent.should eq node
+    verify_dfs(node.left)
+  end
+
+  if node.right.node?
+    node.key.should be < node.right.key
+    (node.red? && node.right.red?).should be_false
+    node.right.parent.should eq node
+    verify_dfs(node.right)
+  end
+end
+
+def verify(set)
+  if set.root.node?
+    set.root.parent.nil_node?.should be_true
+  end
+  verify_dfs(set.root)
+end
+
+describe Set::RedBlackTree(Int32) do
+  it "{}" do
+    S{3, 1, 4, 1, 5}.to_a.should eq [1, 3, 4, 5]
+  end
+
+  it "#root" do
+    s = S.new
+    s.root.nil_node?.should be_true
+  end
+
+  it "#size" do
+    s = S.new
+    s.size.should eq 0
+    s.add 1
+    s.size.should eq 1
+    s.add 1
+    s.size.should eq 1
+    s.delete 1
+    s.size.should eq 0
+  end
+
+  it "#empty?" do
+    s = S.new
+    s.empty?.should be_true
+    s.add 1
+    s.empty?.should be_false
+  end
+
+  it "#clear" do
+    s = S.new
+    s.add 1
+    s.clear.size.should eq 0
+  end
+
+  it "#add?" do
+    s = S.new
+    s.add?(1).should be_true
+    s.add?(1).should be_false
+    verify(s)
+  end
+
+  it "#add, #<<" do
+    s = S.new
+    s.add(1).add(2).add(1)
+    s.size.should eq 2
+    s << 3 << 4 << 3
+    s.size.should eq 4
+    verify(s)
+  end
+
+  it "#min_node, #max_node" do
+    s = S.new
+    s.min_node.nil_node?.should be_true
+    s.max_node.nil_node?.should be_true
+    s << 1 << 2
+    s.min_node.key?.should eq 1
+    s.max_node.key?.should eq 2
+  end
+
+  it "#min?, #min, #max?, #max" do
+    s = S.new
+    s.min?.should be_nil
+    s.max?.should be_nil
+    expect_raises(Set::RedBlackTree::EmptyError) { s.min }
+    expect_raises(Set::RedBlackTree::EmptyError) { s.max }
+    s << 1 << 2
+    s.min?.should eq 1
+    s.max?.should eq 2
+    s.min.should eq 1
+    s.max.should eq 2
+  end
+
+  it "#split" do
+    1000.times do
+      s = S.new(1..10)
+      l, r = s.split(5)
+      l.to_a.should eq [1, 2, 3, 4, 5]
+      r.to_a.should eq [6, 7, 8, 9, 10]
+      verify(s)
+    end
+    10.times do
+      s = S.new(1..1000)
+      l, r = s.split(500)
+      l.to_a.should eq (1..500).to_a
+      r.to_a.should eq (501..1000).to_a
+      verify(s)
+    end
+  end
+
+  it "#each" do
+    s = S{3, 1, 4, 1, 5}
+    a = [] of Int32
+    s.each { |x| a << x }
+    a.should eq [1, 3, 4, 5]
+  end
+
+  it "#reverse_each" do
+    s = S{3, 1, 4, 1, 5}
+    a = [] of Int32
+    s.reverse_each { |x| a << x }
+    a.should eq [5, 4, 3, 1]
+  end
+
+  it "#includes?" do
+    s = S{1, 3, 5}
+    {1, 3, 5}.each { |x| s.includes?(x).should be_true }
+    {0, 2, 4}.each { |x| s.includes?(x).should be_false }
+  end
+
+  it "#search" do
+    s = S{1, 3, 5}
+    {1, 3, 5}.each { |x| s.search(x).key?.should eq x }
+    {0, 2, 4}.each { |x| s.search(x).nil_node?.should be_true }
+  end
+
+  it "#le, #le!" do
+    s = S{1, 3}
+    [nil, 1, 1, 3, 3].each_with_index { |e, x| s.le(x).should eq e }
+    expect_raises(NilAssertionError) { s.le!(0) }
+    [1, 1, 3, 3].each_with_index(1) { |e, x| s.le!(x).should eq e }
+  end
+
+  it "#lt, #lt!" do
+    s = S{1, 3}
+    [nil, nil, 1, 1, 3, 3].each_with_index { |e, x| s.lt(x).should eq e }
+    expect_raises(NilAssertionError) { s.lt!(0) }
+    expect_raises(NilAssertionError) { s.lt!(1) }
+    [1, 1, 3, 3].each_with_index(2) { |e, x| s.lt!(x).should eq e }
+  end
+
+  it "#ge, #ge!" do
+    s = S{1, 3}
+    [1, 1, 3, 3, nil].each_with_index { |e, x| s.ge(x).should eq e }
+    expect_raises(NilAssertionError) { s.ge!(4) }
+    [1, 1, 3, 3].each_with_index { |e, x| s.ge!(x).should eq e }
+  end
+
+  it "#gt, #gt!" do
+    s = S{1, 3}
+    [1, 3, 3, nil, nil].each_with_index { |e, x| s.gt(x).should eq e }
+    expect_raises(NilAssertionError) { s.gt!(3) }
+    expect_raises(NilAssertionError) { s.gt!(4) }
+    [1, 3, 3].each_with_index { |e, x| s.gt!(x).should eq e }
+  end
+
+  it "#to_s, #inspect" do
+    s = S{1, 2, 3, 4}
+    s.to_s.should eq "Set::RedBlackTree{1, 2, 3, 4}"
+    s.inspect.should eq "Set::RedBlackTree{1, 2, 3, 4}"
+  end
+
+  it "big" do
+    n = 10**4
+    s = S.new
+    (1..n).each do |x|
+      s << x
+      s.size.should eq x
+      s.min.should eq 1
+      s.max.should eq x
+      verify(s)
+    end
+    s.to_a.should eq (1..n).to_a
+    (-n..n*2).each do |x|
+      s.includes?(x).should (1 <= x <= n ? be_true : be_false)
+    end
+    (1..n).each do |x|
+      s.delete(x).should be_true
+      s.delete(x).should be_false
+    end
+  end
+end
+
+alias SS = Set::RedBlackTree(String)
+
+it Set::RedBlackTree(String) do
+  SS{"a", "c", "b"}.to_a.should eq %w[a b c]
+  SS{"a", "ab", "abc", "abc"}.to_a.should eq %w[a ab abc]
+end
