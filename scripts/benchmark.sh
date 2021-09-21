964487f9e1a6ac8de6bd9bf3188348f3d5bb52fb
yuruhi <yuruhiya@gmail.com>
2021-09-21 18:39:47 +0900

    fix benchmark.sh

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -6,7 +6,10 @@ for file in `find ./benchmarks -name "*.cr" | grep -v helper | sort`; do
 	url=https://github.com/yuruhi/crystal_lib/blob/master/${file#./}
 
 	result=`crystal $file --release`
+
 	echo "$result"
+	echo
+	
 	cat - <<- EOS >> $readme
 	## [${file#./benchmarks/}]($url)
 
