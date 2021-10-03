c6aae37110769e5adf81c265d37a6b69d5322268
yuruhi <yuruhiya@gmail.com>
2021-10-03 13:40:48 +0900

    add benchmark for scanner

---
+++ benchmarks/scanner.cr
@@ -0,0 +1,30 @@
+require "../src/scanner"
+require "benchmark"
+
+N = 100000
+
+{% for signed in [true, false] %}
+  {% for num in [8, 16, 32, 64] %}
+    {%
+      type = signed ? "Int#{num}".id : "UInt#{num}".id
+      method = signed ? "i#{num}".id : "u#{num}".id
+    %}
+    a_{{method}} = Array.new(N) { rand({{type}}) }
+    tmp_{{method}} = File.tempfile { |file| a_{{method}}.join(' ', file) }
+
+    Benchmark.ips do |x|
+      x.report("Scanner.{{method}}".rjust(16)) do
+        file = File.new tmp_{{method}}.path
+        b = Array.new(N) { Scanner.{{method}}(file) }
+        raise "" unless a_{{method}} == b
+      end
+      x.report("Scanner.s.to_{{method}}") do
+        file = File.new tmp_{{method}}.path
+        b = Array.new(N) { Scanner.s(file).to_{{method}} }
+        raise "" unless a_{{method}} == b
+      end
+    end
+
+    tmp_{{method}}.delete
+  {% end %}
+{% end %}
