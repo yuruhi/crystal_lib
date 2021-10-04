0af6b24939b76c6d4e231dcf3c9a46a4147f82a5
yuruhi <yuruhiya@gmail.com>
2021-10-04 21:12:02 +0900

    fix benchmark for `BinaryHeap`

--- benchmarks/datastructure/binary_heap.cr
+++ benchmarks/datastructure/binary_heap.cr
@@ -1,7 +1,23 @@
 require "benchmark"
 require "../../src/datastructure/binary_heap"
 
-def benchmark(label, values : Array(T)) forall T
+private class SlowCmp
+  include Comparable(SlowCmp)
+
+  def initialize(size)
+    @array = Array(Int32).new(size) { yield }
+  end
+
+  def initialize
+    @array = [] of Int32
+  end
+
+  def <=>(other : SlowCmp)
+    @array.sum <=> other.@array.sum
+  end
+end
+
+private def benchmark(label, values : Array(T)) forall T
   Benchmark.ips do |x|
     x.report(label) do
       a = BinaryHeap(T).new
@@ -12,10 +28,23 @@ def benchmark(label, values : Array(T)) forall T
   end
 end
 
-benchmark "Int32 10^2 sparse", Array.new(10**2) { |i| i }
-benchmark "Int32 10^2 dence ", Array.new(10**2) { |i| i % 10 }
-benchmark "Int32 10^6 sparse", Array.new(10**6) { |i| i }
-benchmark "Int32 10^6 dence ", Array.new(10**6) { |i| i % 100 }
+private def benchmark_reverse(label, values : Array(T)) forall T
+  Benchmark.ips do |x|
+    x.report(label) do
+      a = BinaryHeap(T).new { |a, b| b <=> a }
+      values.each { |x| a.add x }
+      values.size.times { a.pop }
+      raise "" unless a.empty?
+    end
+  end
+end
 
 r = Random.new(12345)
-benchmark "Array 10^6 * 10^2", Array.new(10**6) { Array.new(10**2) { r.rand(100) } }
+
+benchmark "Int32 10^2 sparse  ", Array.new(10**2) { |i| i }
+benchmark "Int32 10^2 dence   ", Array.new(10**2) { |i| i % 10 }
+benchmark "Int32 10^6 sparse  ", Array.new(10**6) { |i| i }
+benchmark "Int32 10^6 dence   ", Array.new(10**6) { |i| i % 100 }
+benchmark_reverse "Int32 10^6 sparse R", Array.new(10**6) { |i| i }
+benchmark_reverse "Int32 10^6 dence  R", Array.new(10**6) { |i| i % 100 }
+benchmark "SlowC 10^6 * 10^2  ", Array.new(10**6) { SlowCmp.new(10**2) { r.rand(100) } }
