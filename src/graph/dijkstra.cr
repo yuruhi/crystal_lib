4046680ab2a2718d3414d865f4b89f3696979c38
yuruhi <yuruhiya@gmail.com>
2022-01-01 11:25:12 +0900

    add `Graph#dijkstra(start, goal)`

--- src/graph/dijkstra.cr
+++ src/graph/dijkstra.cr
@@ -1,17 +1,17 @@
-require "../../atcoder/src/PriorityQueue"
+require "../datastructure/binary_heap"
 require "../graph"
 
 module Graph(Edge, Edge2)
   # Returns the array of distance of each node from *start* or `nil`.
   def dijkstra(start : Int)
     raise ArgumentError.new unless 0 <= start < size
-    que = AtCoder::PriorityQueue({Int32, typeof(first.cost)}).new { |(v1, d1), (v2, d2)| d1 > d2 }
+    que = BinaryHeap({Int32, typeof(first.cost)}).new { |(v1, d1), (v2, d2)| d1 <=> d2 }
     que << {start, typeof(first.cost).zero}
     dist = Array(typeof(first.cost)?).new(size, nil)
     dist[start] = typeof(first.cost).zero
 
-    while vd = que.pop
-      v, d = vd
+    until que.empty?
+      v, d = que.pop
       next if dist[v].try { |dist_v| dist_v < d }
       current_dist = dist[v].not_nil!
       graph[v].each do |edge|
@@ -28,6 +28,34 @@ module Graph(Edge, Edge2)
   # Returns the array of distance of each node from *start*.
   def dijkstra!(start : Int)
     dijkstra(start).map(&.not_nil!)
+  end
+
+  # Returns the distance of *start* to *goal* or `nil`.
+  def dijkstra(start : Int, goal : Int)
+    raise ArgumentError.new unless 0 <= start < size && 0 <= goal < size
+    que = BinaryHeap({Int32, typeof(first.cost)}).new { |(v1, d1), (v2, d2)| d1 <=> d2 }
+    que << {start, typeof(first.cost).zero}
+    dist = Array(typeof(first.cost)?).new(size, nil)
+    dist[start] = typeof(first.cost).zero
+
+    until que.empty?
+      v, d = que.pop
+      return d if v == goal
+      next if dist[v].try { |dist_v| dist_v < d }
+      current_dist = dist[v].not_nil!
+      graph[v].each do |edge|
+        next_dist = current_dist + edge.cost
+        if dist[edge.to].nil? || dist[edge.to].not_nil! > next_dist
+          dist[edge.to] = next_dist
+          que << {edge.to, next_dist}
+        end
+      end
+    end
+  end
+
+  # Returns the distance of *start* to *goal*.
+  def dijkstra!(start : Int, goal : Int)
+    dijkstra(start, goal).not_nil!
   end
 
   def dijkstra_with_prev(start : Int)
