d41f397bf2911b90fca89064a0a66ce6b397cc28
yuruhi <yuruhiya@gmail.com>
2022-01-02 17:13:44 +0900

    fix

--- src/math/sieve_fast.cr
+++ src/math/sieve_fast.cr
@@ -1,9 +1,6 @@
 class SieveFast
   getter size : Int32, factor : Array(Int32), primes : Array(Int32)
 
-  # i      :  0  1  2  3  4  5  6
-  # factor :  3  5  7  9 11 13 15
-
   private def value(i)
     2 * i + 3
   end
@@ -11,6 +8,10 @@ class SieveFast
   private def index(x)
     (x - 3) // 2
   end
+
+  # index :  0  1  2  3  4  5  6  7  8  9 10 11
+  # value :  3  5  7  9 11 13 15 17 19 21 23 25
+  # factor:  3  5  7  3 11 13  3 17 19  3 23  5
 
   private def sift(n)
     # factor(i)       = value(i)          = 2i + 3
