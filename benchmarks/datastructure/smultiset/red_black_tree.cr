42ffac8769b727a2c25b20e3219cea6151cc59b6
yuruhi <yuruhiya@gmail.com>
2022-01-16 12:19:16 +0900

    fix compile error

--- benchmarks/datastructure/smultiset/red_black_tree.cr
+++ benchmarks/datastructure/smultiset/red_black_tree.cr
@@ -1,4 +1,6 @@
 require "benchmark"
 require "../sset_benchmark_helper"
 require "../../../src/datastructure/smultiset/red_black_tree"
-benchmark_sset(SMultiSet::RedBlackTree)
+benchmark_sset_add_delete(SMultiSet::RedBlackTree)
+puts
+benchmark_sset_split(SMultiSet::RedBlackTree)
--- benchmarks/datastructure/smultiset/treap.cr
+++ benchmarks/datastructure/smultiset/treap.cr
@@ -1,4 +1,5 @@
-require "benchmark"
 require "../../../src/datastructure/smultiset/treap"
 require "../sset_benchmark_helper"
-benchmark_sset(SMultiSet::Treap)
+benchmark_sset_add_delete(SMultiSet::Treap)
+puts
+benchmark_sset_split(SMultiSet::Treap)
