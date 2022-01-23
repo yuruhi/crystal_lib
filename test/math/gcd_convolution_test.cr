05b8f7b533fce40aacbf2a5feabf12390ca6c047
yuruhi <yuruhiya@gmail.com>
2022-01-23 12:21:56 +0900

    fix compile error

--- test/math/gcd_convolution_test.cr
+++ test/math/gcd_convolution_test.cr
@@ -2,7 +2,7 @@
 require "../../src/math/gcd"
 require "../../src/math/mint"
 h, w = read_line.split.map(&.to_i)
-hh = (0...h).map { |i| (h - i).to_m }
-ww = (0...w).map { |i| (w - i).to_m }
-one = h.to_m * w.pred + w.to_m * h.pred
+hh = (0...h).map { |i| Mint.new h - i }
+ww = (0...w).map { |i| Mint.new w - i }
+one = Mint.new(h) * w.pred + Mint.new(w) * h.pred
 puts GCD.convolution(hh, ww).fetch(1, Mint.zero) * 2 + one
