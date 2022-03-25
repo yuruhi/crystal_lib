3177688568affb974f8a608ff8f0210001dab8ff
yuruhi <yuruhiya@gmail.com>
2022-03-25 19:23:03 +0900

    :hammer:

--- src/graph/topological_sort.cr
+++ src/graph/topological_sort.cr
@@ -4,13 +4,13 @@ require "./degree"
 module Graph(Edge, Edge2)
   def topological_sort : Array(Int32)?
     deg = indegree
-    que = Deque(Int32).new
-    size.times { |i| que << i if deg[i] == 0 }
+    queue = Deque(Int32).new
+    size.times { |i| queue << i if deg[i] == 0 }
     result = Array(Int32).new(size)
-    while v = que.shift?
+    while v = queue.shift?
       result << v
       self[v].each do |edge|
-        que << edge.to if (deg[edge.to] -= 1) == 0
+        queue << edge.to if (deg[edge.to] -= 1) == 0
       end
     end
     deg.any? { |x| x > 0 } ? nil : result
