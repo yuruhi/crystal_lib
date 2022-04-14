129c22d222bde1932c2639d9b506a8632adafd63
yuruhi <yuruhiya@gmail.com>
2022-04-14 18:21:37 +0900

    :shirt:

--- src/tuple/get.cr
+++ src/tuple/get.cr
@@ -1,10 +1,10 @@
 struct Tuple
   macro method_missing(call)
-		{% str = call.stringify %}
-		{% if str =~ /_\d+/ %}
-		  self[{{ str[1...str.size].id }}]
-		{% else %}
-			{% raise call %}
-		{% end %}
-	end
+    {% str = call.stringify %}
+    {% if str =~ /_\d+/ %}
+      self[{{ str[1...str.size].id }}]
+    {% else %}
+      {% raise call %}
+    {% end %}
+  end
 end
