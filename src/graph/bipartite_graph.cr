0afc36072e7d112491f599f048467fcc1e95386a
yuruhi <yuruhiya@gmail.com>
2022-04-16 15:43:23 +0900

    :hammer:

--- src/graph/bipartite_graph.cr
+++ src/graph/bipartite_graph.cr
@@ -3,20 +3,20 @@ require "../graph"
 module Graph(Edge, Edge2)
   # Returns table of color if the graph is bipartite graph, otherwise returns `nil`
   def bipartite_graph : Array(Bool)?
-    table = Array(Bool?).new(size, nil)
+    color = Array(Bool?).new(size, nil)
     queue = Deque.new (0...size).to_a
     while v = queue.shift?
-      table[v] = true if table[v].nil?
-      flag = table[v].not_nil!
+      c1 = color[v]
+      c1 = color[v] = true if c1.nil?
       graph[v].each do |edge|
-        if (flag2 = table[edge.to]).nil?
-          table[edge.to] = !flag
+        if (c2 = color[edge.to]).nil?
+          color[edge.to] = !c1
           queue.unshift edge.to
         else
-          return nil if flag == flag2
+          return nil if c1 == c2
         end
       end
     end
-    table.map(&.not_nil!)
+    color.map(&.not_nil!)
   end
 end
