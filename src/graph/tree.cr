8e6d966ccdc63abbe03520fe5535a487275cc991
yuruhi <yuruhiya@gmail.com>
2022-04-16 16:26:58 +0900

    :hammer: graph/tree

--- src/graph/tree.cr
+++ src/graph/tree.cr
@@ -1,29 +1,30 @@
 require "../graph"
 
 module Graph(Edge, Edge2)
-  private def subtree_size_dfs(v : Int32, p : Int32, result : Array(Int32)) : Int32
-    result[v] = 1 + self[v].sum { |edge|
-      edge.to != p ? subtree_size_dfs(edge.to, v, result) : 0
-    }
+  private def subtree_size_dfs(v : Int32, p : Int32, a : Array(Int32)) : Int32
+    each_child(v, p) do |edge|
+      a[v] += subtree_size_dfs(edge.to, v, a)
+    end
+    a[v] += 1
   end
 
   def subtree_size(root : Int32) : Array(Int32)
-    result = Array.new(size, 0)
-    subtree_size_dfs(root, -1, result)
-    result
+    a = Array.new(size, 0)
+    subtree_size_dfs(root, -1, a)
+    a
   end
 
-  private def tree_distance_dfs(v : Int32, p : Int32, dist : Int32, a) : Nil
+  private def tree_distance_dfs(v : Int32, p : Int32, dist, a) : Nil
     a[v] = dist
-    self[v].each do |edge|
-      tree_distance_dfs(edge.to, v, dist + edge.cost, a) if edge.to != p
+    each_child(v, p) do |edge|
+      tree_distance_dfs(edge.to, v, dist + edge.cost, a)
     end
   end
 
   # Returns the distance of each node from *root*.
   def tree_distance(root : Int32)
     a = Array.new(size, typeof(first.cost).zero)
-    tree_distance_dfs(root, -1, 0, a)
+    tree_distance_dfs(root, -1, typeof(first.cost).zero, a)
     a
   end
 
@@ -34,17 +35,17 @@ module Graph(Edge, Edge2)
     {dist, v, u}
   end
 
-  private def parent_table_dfs(v : Int32, p : Int32?, table : Array(Int32?)) : Nil
-    table[v] = p
-    self[v].each do |edge|
-      parent_table_dfs(edge.to, v, table) if edge.to != p
+  private def parent_table_dfs(v : Int32, p : Int32?, a : Array(Int32?)) : Nil
+    a[v] = p
+    each_child(v, p) do |edge|
+      parent_table_dfs(edge.to, v, a)
     end
   end
 
   def parent_table(root : Int32) : Array(Int32?)
-    table = Array(Int32?).new(size, nil)
-    parent_table_dfs(root, nil, table)
-    table
+    a = Array(Int32?).new(size, nil)
+    parent_table_dfs(root, nil, a)
+    a
   end
 
   def tree_path(start : Int32, goal : Int32, &) : Nil
