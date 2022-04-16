39f50daf50618825c2ab82d372215ff2d5f6cb26
yuruhi <yuruhiya@gmail.com>
2022-04-16 15:37:04 +0900

    :sparkles: `Graph#bfs01_st!`

--- src/graph/bfs01.cr
+++ src/graph/bfs01.cr
@@ -6,15 +6,15 @@ module Graph(Edge, Edge2)
   def bfs01(start : Int, &block) : Array(Int32?)
     raise ArgumentError.new unless 0 <= start < size
     queue = Deque{start}
-    dist = Array(Int32?).new(size, nil)
-    dist[start] = 0
+    dist = Array(Int32?).new(size, nil).tap(&.[start] = 0)
     while v = queue.shift?
       current_dist = dist[v].not_nil!
       graph[v].each do |edge|
-        cost = yield(edge)
+        cost = yield edge
         raise ArgumentError.new("Unexpected cost: #{cost}") unless cost == 0 || cost == 1
         next_dist = current_dist + cost
-        if (d = dist[edge.to]).nil? || next_dist < d.not_nil!
+        d = dist[edge.to]
+        if d.nil? || next_dist < d
           dist[edge.to] = next_dist
           if cost == 0
             queue.unshift edge.to
@@ -50,16 +50,16 @@ module Graph(Edge, Edge2)
   def bfs01_st(start : Int, goal : Int, &block) : Int32?
     raise ArgumentError.new unless 0 <= start < size
     queue = Deque{start}
-    dist = Array(Int32?).new(size, nil)
-    dist[start] = 0
+    dist = Array(Int32?).new(size, nil).tap(&.[start] = 0)
     while v = queue.shift?
       current_dist = dist[v].not_nil!
       return current_dist if v == goal
       graph[v].each do |edge|
-        cost = yield(edge)
+        cost = yield edge
         raise ArgumentError.new("Unexpected cost: #{cost}") unless cost == 0 || cost == 1
         next_dist = current_dist + cost
-        if (d = dist[edge.to]).nil? || next_dist < d.not_nil!
+        d = dist[edge.to]
+        if d.nil? || next_dist < d
           dist[edge.to] = next_dist
           if cost == 0
             queue.unshift edge.to
@@ -76,5 +76,17 @@ module Graph(Edge, Edge2)
   # All of cost of edge must be `0` or `1`.
   def bfs01_st(start : Int, goal : Int) : Int32?
     bfs01_st(start, goal, &.cost)
+  end
+
+  # Returns the distance from *start* to *goal* if path is present, otherwise raises `ArgumentError`.
+  # *block* must return `0` or `1`.
+  def bfs01_st!(start : Int, goal : Int, &block) : Int32
+    bfs01_st(start, goal) { |edge| yield edge } || raise ArgumentError.new
+  end
+
+  # Returns the distance from *start* to *goal* if path is present, otherwise raises `ArgumentError`.
+  # All of cost of edge must be `0` or `1`.
+  def bfs01_st!(start : Int, goal : Int) : Int32
+    bfs01_st(start, goal, &.cost) || raise ArgumentError.new
   end
 end
--- test/graph/bfs01_test.cr
+++ test/graph/bfs01_test.cr
@@ -5,8 +5,9 @@ n, m, k = input(i, i, i)
 graph = UnGraph.new n, input({i - 1, i - 1, i}[m])
 puts (0..2*10**5).bsearch { |x|
   d1 = graph.bfs01_st(0, n - 1) { |edge| edge.cost > x ? 1 : 0 }.not_nil!
-  d2 = graph.bfs01(0) { |edge| edge.cost > x ? 1 : 0 }[n - 1].not_nil!
-  d3 = graph.bfs01!(0) { |edge| edge.cost > x ? 1 : 0 }[n - 1]
-  raise "" unless d1 == d2 == d3
+  d2 = graph.bfs01_st!(0, n - 1) { |edge| edge.cost > x ? 1 : 0 }
+  d3 = graph.bfs01(0) { |edge| edge.cost > x ? 1 : 0 }.last.not_nil!
+  d4 = graph.bfs01!(0) { |edge| edge.cost > x ? 1 : 0 }.last
+  raise "" unless d1 == d2 == d3 == d4
   d1 < k
 }
