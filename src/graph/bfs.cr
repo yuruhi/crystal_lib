a7ae6ad66691a94fe155c0218fd40f5b8a087767
yuruhi <yuruhiya@gmail.com>
2022-04-16 15:36:30 +0900

    :hammer:

--- src/graph/bfs.cr
+++ src/graph/bfs.cr
@@ -5,8 +5,7 @@ module Graph(Edge, Edge2)
   def bfs(start : Int) : Array(Int32?)
     raise ArgumentError.new unless 0 <= start < size
     queue = Deque{start}
-    dist = Array(Int32?).new(size, nil)
-    dist[start] = 0
+    dist = Array(Int32?).new(size, nil).tap(&.[start] = 0)
     while v = queue.pop?
       graph[v].each do |edge|
         if dist[edge.to].nil?
@@ -27,8 +26,7 @@ module Graph(Edge, Edge2)
   def bfs_st(start : Int, goal : Int) : Int32?
     raise ArgumentError.new unless 0 <= start < size && 0 <= goal < size
     queue = Deque{start}
-    dist = Array(Int32?).new(size, nil)
-    dist[start] = 0
+    dist = Array(Int32?).new(size, nil).tap(&.[start] = 0)
     while v = queue.pop?
       d = dist[v].not_nil!
       return d if v == goal
@@ -39,6 +37,5 @@ module Graph(Edge, Edge2)
         end
       end
     end
-    nil
   end
 end
